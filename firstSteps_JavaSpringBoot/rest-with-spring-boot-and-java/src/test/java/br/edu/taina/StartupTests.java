fd99b61351a7963da465b42d9872f4fb6a9a9f1b
Tainá <tainamenezescris@gmail.com>
2025-01-20 22:10:37 -0300

    First steps in Java with Spring Boot

---
+++ firstSteps_JavaSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/edu/taina/StartupTests.java
@@ -0,0 +1,13 @@
+package br.edu.taina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

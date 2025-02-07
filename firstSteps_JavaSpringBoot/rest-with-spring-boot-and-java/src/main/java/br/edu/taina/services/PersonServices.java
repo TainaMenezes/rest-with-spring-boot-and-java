8ce4e13e44ea444cc08d54184520368ee664d7e1
Tainá <tainamenezescris@gmail.com>
2025-02-06 21:28:02 -0300

    Conexão com o banco de dados MySQL, criação de crud de pessoa.

---
+++ firstSteps_JavaSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/edu/taina/services/PersonServices.java
@@ -0,0 +1,63 @@
+package br.edu.taina.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.taina.exceptions.ResourceNotFoundException;
+import br.edu.taina.model.Person;
+import br.edu.taina.repositories.PersonRepository;
+
+@Service
+public class PersonServices {
+	
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public List<Person> findAll() {
+		logger.info("Finding all people.");
+		
+		return repository.findAll();
+	}
+
+	public Person findById(Long id) {
+		logger.info("Finding one person.");
+		
+		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID."));
+	}
+	
+	public Person create(Person person) {
+		logger.info("Creating one person");
+		return repository.save(person);
+	}
+	
+	public Person update(Person person) {
+		logger.info("Updating one person");
+		
+		Person entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID."));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGenre(person.getGenre());
+		
+		return repository.save(entity);
+	}
+	
+	public void delete(Long id) {
+		logger.info("deleting one person");
+		
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID."));
+		
+		repository.delete(entity);
+	}
+
+}
